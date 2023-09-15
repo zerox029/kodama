fae52d28dab64be5bf376ce7d96beb2e775431ae
Emma Plante <zerox029@gmail.com>
2023-09-14 20:41:07 -0400

    WIP on parser

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "parser.hpp"
+#include <utility>
+
+Parser::Parser(std::queue<Token> tokenQueue) : tokens {std::move(tokenQueue)}, currentToken {tokens.front()} {
+  tokens.pop();
+}
+
+std::shared_ptr<AstNode>
+Parser::Parse() {
+
+}
+
+void
+Parser::advance() {
+  currentToken = tokens.front();
+  tokens.pop();
+}
+
+std::shared_ptr<AstNode>
+Parser::ParseBinaryOperation() {
+  std::shared_ptr<AstNode> lhs = ParseNumber();
+}
+
+std::shared_ptr<AstNode>
+Parser::ParseNumber() {
+  NumberLiteral numberLiteralNode {currentToken, currentToken.getStr()};
+
+  advance();
+
+  return std::make_shared<NumberLiteral>(numberLiteralNode);
+}
--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -6,17 +6,24 @@
 #define KODAMA_SRC_PARSER_PARSER_HPP_
 
 #include "../lexer/token.hpp"
+#include "../ast/ast.hpp"
 #include <queue>
 #include <utility>
+#include <memory>
 
 class Parser {
  public:
-  explicit Parser(std::queue<Token> tokens) : tokens{std::move(tokens)} {}
-
-  
+  explicit Parser(std::queue<Token> tokens);
+  std::shared_ptr<AstNode> Parse();
 
  private:
   std::queue<Token> tokens;
+  Token currentToken;
+
+  void advance();
+
+  std::shared_ptr<AstNode> ParseBinaryOperation();
+  std::shared_ptr<AstNode> ParseNumber();
 };
 
 #endif //KODAMA_SRC_PARSER_PARSER_HPP_
